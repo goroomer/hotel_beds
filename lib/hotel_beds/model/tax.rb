8a679c11ef578c251a86f983f345d2af4d93e36e
shanys8 <shany@roomertravel.com>
2015-07-28 15:50:55 +0300

    removed validataions

--- lib/hotel_beds/model/tax.rb
+++ lib/hotel_beds/model/tax.rb
@@ -9,9 +9,10 @@ module HotelBeds
       attribute :percent, BigDecimal
       attribute :import, BigDecimal
 
-      validates :included, length: { is: 1 }
-      validates :percent, numericality: { greater_than: 0, only_integer: true }
-      validates :percent, import: { greater_than: 0, only_integer: true }
+      # validation
+      # validates :included, length: { is: 1 }
+      # validates :percent, numericality: { greater_than: 0, only_integer: true }
+      # validates :percent, import: { greater_than: 0, only_integer: true }
     end
   end
 end
