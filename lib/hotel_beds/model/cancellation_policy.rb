afa2d04bf8131467156be63af532a730ede7be4e
shanys8 <shany@roomertravel.com>
2015-08-12 12:23:14 +0300

    moved code to model from parser

--- lib/hotel_beds/model/cancellation_policy.rb
+++ lib/hotel_beds/model/cancellation_policy.rb
@@ -9,6 +9,18 @@ module HotelBeds
       attribute :amount, BigDecimal
       attribute :from_date, String
       attribute :from_time, String
+
+      def from_date=(str)
+        v = str.match(/^(\d{4})(\d{2})(\d{2})$/)
+        super("#{v[1]}-#{v[2]}-#{v[3]}")
+      end
+
+      def from_time=(str)
+        v = str.match(/^(\d{2})(\d{2})$/)
+        super("#{v[1]}:#{v[2]}")
+      end
+
+
     end
   end
 end
--- lib/hotel_beds/parser/cancellation_policy.rb
+++ lib/hotel_beds/parser/cancellation_policy.rb
@@ -5,20 +5,9 @@ module HotelBeds
     class CancellationPolicy
       include HotelBeds::Parser
 
-
       attribute :amount, attr: "amount"
       attribute :from_date, attr: "dateFrom"
       attribute :from_time, attr: "time"
-
-      def from_date=(str)
-        v = str.match(/^(\d{4})(\d{2})(\d{2})$/)
-        super("#{v[1]}-#{v[2]}-#{v[3]}")
-      end
-
-      def from_time=(str)
-        v = str.match(/^(\d{2})(\d{2})$/)
-        super("#{v[1]}:#{v[2]}")
-      end
 
     end
   end
